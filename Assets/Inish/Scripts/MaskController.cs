32e1771527ff12c4801dd4ea7f956871f771e569
icarus <theanchor45@gmail.com>
2026-01-29 11:40:55 +0530

    Update MaskController.cs

--- Assets/Inish/Scripts/MaskController.cs
+++ Assets/Inish/Scripts/MaskController.cs
@@ -1,16 +1,61 @@
+using System;          // REQUIRED for Action
 using UnityEngine;
+
+public enum MaskState
+{
+    Physical,
+    Spirit
+}
 
 public class MaskController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static MaskController Instance;
+
+    [Header("Cameras")]
+    public Camera PhysicalCamera;
+    public Camera SpiritCamera;
+
+    public MaskState CurrentState { get; private set; }
+
+    public event Action<MaskState> OnMaskStateChanged;
+
+    void Awake()
     {
-        
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        SetState(MaskState.Physical);
+    }
+
     void Update()
     {
-        
+        // Free toggle input
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ToggleMask();
+        }
+    }
+
+    public void ToggleMask()
+    {
+        if (CurrentState == MaskState.Physical)
+            SetState(MaskState.Spirit);
+        else
+            SetState(MaskState.Physical);
+    }
+
+    public void SetState(MaskState newState)
+    {
+        CurrentState = newState;
+
+        PhysicalCamera.enabled = (newState == MaskState.Physical);
+        SpiritCamera.enabled   = (newState == MaskState.Spirit);
+
+        OnMaskStateChanged?.Invoke(newState);
     }
 }
