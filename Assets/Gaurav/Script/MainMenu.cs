f23ff8cce37005bd4dbcaba99e7d76d684dba5b8
Gaurav Rajendra Lad <grlad265@gmail.com>
2026-01-31 17:41:55 +0530

    UI

---
+++ Assets/Gaurav/Script/MainMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [Header("Scene")]
+    public string gameSceneName = "Game"; // your gameplay scene name
+
+    // PLAY BUTTON
+    public void PlayGame()
+    {
+        Debug.Log("Play");
+        //SceneManager.LoadScene(" ");
+    }
+
+    // QUIT BUTTON
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+
+        Application.Quit();
+
+        // Stop play mode inside Unity Editor
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
