6a34376647265bc916dfa983a71d5072ab151ae1
Gaurav Rajendra Lad <grlad265@gmail.com>
2026-01-31 19:03:16 +0530

    UI Done

---
+++ Assets/Gaurav/Script/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject pausePanel;
+    public GameObject mainMenuPanel;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !mainMenuPanel.activeSelf)
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    // CONTINUE
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    // PAUSE
+    void Pause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    // QUIT → go back to main menu panel
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+
+        pausePanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+
+        isPaused = false;
+    }
+}
